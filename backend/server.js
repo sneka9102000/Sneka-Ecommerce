4383b2c7a7c5a42eb3e094c423c633fd9eebf304
Sneka9102000 <snekakumar9252gmail.com>
2022-06-22 11:20:35 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ const app =require("./app");
 const dotenv=require("dotenv");
 const connectDatabase = require("./config/database")
 
+//Handlin uncaught exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Uncaught Exception`);
+    process.exit(1);
+});
+
 //config
 
 dotenv.config({path:"backend/config/config.env"})
@@ -11,7 +18,20 @@ dotenv.config({path:"backend/config/config.env"})
 
 connectDatabase()
 
-app.listen(process.env.PORT,()=> {
+const server =app.listen(process.env.PORT,()=> {
 
-    console.log(`Server is working on http://localhost:${process.env.port}`)
-})
+    console.log(`Server is working on http://localhost:${process.env.port}`);
+});
+console.log(youtube)
+
+//unhandled promise rejection
+
+process.on("unhandledRejection", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to Unhandled Promise Rejection`);
+  
+    server.close(() => {
+      process.exit(1);
+    });
+  });
+  
