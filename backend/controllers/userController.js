531932b3a1f81957ae3f18e8975f5981c8bc78ab
sneka9102000 <snekakumar925@gmail.com>
2022-06-23 17:13:48 +0530

    commit

---
+++ backend/controllers/userController.js
@@ -0,0 +1,25 @@
+const ErrorHander = require("../utils/errorhandler");
+const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const User = require("../models/userModel");
+
+// Register a User
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const {name,email,password} = req.body;
+
+  const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+          public_id:"this_is_sample",
+          url: "picurl",
+        },
+    });
+
+    const token = user.getJWTToken();
+
+    res.status(200).json({
+        success: true,
+        token,
+      });
+    });
