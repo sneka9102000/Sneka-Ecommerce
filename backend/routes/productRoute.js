30624c692f0528f19e0d68136de1a2e8b085de6a
sneka9102000 <snekakumar925@gmail.com>
2022-06-26 13:38:53 +0530

    commit

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -5,7 +5,9 @@ const router = express.Router ();
 
 router.route("/products").get(getAllProducts)
 router.route("/product/new").post(isAuthenticatedUser,authorizeRoles("admin"),createProduct)
-router.route("/product/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct).get(isAuthenticatedUser,getProductDetails)
+router.route("/product/:id").put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
+
+router.route("/product/:id").get(getProductDetails);
 
 
 module.exports = router
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const { registerUser, loginUser,logout, getUserDetails, updatePassword } = require("../controllers/userController");
-const { isAuthenticatedUser,authorizesRoles } = require("../middleware/auth")
+const { registerUser, loginUser,logout, getUserDetails, updatePassword, updateProfile, getAllUser, getSingleUser, updateUserRole, deleteUser } = require("../controllers/userController");
+const { isAuthenticatedUser,authorizeRoles } = require("../middleware/auth")
 
 const router = express.Router();
 
@@ -9,6 +9,11 @@ router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticatedUser,getUserDetails);
 router.route("/password/update").put(isAuthenticatedUser,updatePassword);
+router.route("/me/profile").put(isAuthenticatedUser,updateProfile);
+router.route("/admin/users").get(isAuthenticatedUser,authorizeRoles("admin"),getAllUser);
+router.route("/admin/user/:id").get(isAuthenticatedUser,authorizeRoles("admin"),getSingleUser).put(isAuthenticatedUser,authorizeRoles("admin"),updateUserRole).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteUser);
+
+
 
 
 
