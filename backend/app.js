0b8ea3fe1e234bee2a4a377def0a7e34416f3d36
sneka9102000 <snekakumar925@gmail.com>
2022-06-27 09:34:31 +0530

    commit

--- backend/app.js
+++ backend/app.js
@@ -10,10 +10,12 @@ app.use(cookieParser());
 
 const product = require("./routes/productRoute");
 const user = require("./routes/userRoute");
+const order = require("./routes/orderRoutes");
+
 
 app.use("/api/v1",product);
-app.use("/api/v1",user)
-
+app.use("/api/v1",user);
+app.use("/api/v1",order);
 //middleware for errors
 
 app.use(errorMiddleware);
