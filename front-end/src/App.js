0439d15fa3251f783dd7904912205f475e3a7031
sneka9102000 <snekakumar925@gmail.com>
2022-06-28 12:11:42 +0530

    commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,27 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import ReactNavbar from "overlay-navbar/dist/lib/ReactNavbar";
-import "overlay-navbar/dist/lib/ReactNavbar.min.css";
-import Header from "./component/layout/Header";
+// import ReactNavbar from "overlay-navbar/dist/lib/ReactNavbar";
+// import "overlay-navbar/dist/lib/ReactNavbar.min.css";
+import Header from "./component/layout/Header/Header.js";
+import WebFont from "webfontloader"
+import React from "react";
+// import Footer from "./component/layout/Footer/Footer.js"
 
-export default function App() {
+
+function App(){
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Roboto", "Droid Sans", "Chilanka"],
+      }
+    })
+    }, [])  
+
     return (
     <Router>
       <Header/>
+      {/* <Footer /> */}
     </Router>
   );
 }
+export default App;
+  
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import "overlay-navbar/dist/lib/ReactNavbar.min.css";
 
 import App from "./App";
 
